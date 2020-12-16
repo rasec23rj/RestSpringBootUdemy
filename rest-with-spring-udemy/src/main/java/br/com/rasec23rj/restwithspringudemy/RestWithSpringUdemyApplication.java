43088fd8e01f9d535eab33e914f479221482273d
rasec23rj <rasec23rj@gmail.com>
2020-12-16 16:09:44 -0300

    projeto

---
+++ rest-with-spring-udemy/src/main/java/br/com/rasec23rj/restwithspringudemy/RestWithSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.rasec23rj.restwithspringudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringUdemyApplication.class, args);
+	}
+
+}
---
+++ rest-with-spring-udemy/src/test/java/br/com/rasec23rj/restwithspringudemy/RestWithSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rasec23rj.restwithspringudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
