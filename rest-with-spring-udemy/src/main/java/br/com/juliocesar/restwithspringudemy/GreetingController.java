3c59694ab081800dd67966fff945b91216d7f709
rasec23rj <rasec23rj@gmail.com>
2020-12-17 10:50:54 -0300

    methodo sum

--- rest-with-spring-udemy/src/main/java/br/com/juliocesar/restwithspringudemy/GreetingController.java
+++ rest-with-spring-udemy/src/main/java/br/com/juliocesar/restwithspringudemy/GreetingController.java
@@ -5,7 +5,9 @@ package br.com.juliocesar.restwithspringudemy;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,26 @@ public class GreetingController {
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@RequestMapping(value="/sum/numberOne/numberTwo", method=RequestMethod.GET)
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		Double sum = convertToDouble(numberOne)  + convertToDouble(numberTwo);
+		return 1D;
+	}
+
+	private Double convertToDouble(String numberOne) {
+		// TODO Auto-generated method stub
+		return 1D;
+	}
+
+	private boolean isNumeric(String number) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
